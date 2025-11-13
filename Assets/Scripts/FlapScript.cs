a9c38ff6bf94d87e2be36f52740b0559192eced7
eclipse2101 <rutherfordd2101@gmail.com>
2025-11-13 12:42:57 -0500

    Made the flips work
    The flaps for the pinball game is now working. Now all I gotta do is fix the speed of them and bind the controls to it

--- Assets/Scripts/FlapScript.cs
+++ Assets/Scripts/FlapScript.cs
@@ -1,26 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem; 
 
 public class FlapScript : MonoBehaviour
 {
     public float flapup;
     public float flapdown;
     public float rFlap;
+    public float speed;
 
+    private Vector3 vup;
+    private Vector3 vdown; 
     // Start is called before the first frame update
     void Awake()
     {
         if(gameObject.CompareTag("Left Flap"))
         {
-            flapdown = (int)gameObject.transform.rotation.eulerAngles.z;
+            flapdown = (int)gameObject.transform.rotation.eulerAngles.z;//330f
             flapup = 30f; //This pretty much acts as postive 30 in the inspector 
+            vup = Vector3.forward;
+            vdown = Vector3.back; 
             Debug.Log("This is left flap");
         }
         else if(gameObject.CompareTag("Right Flap"))
         {
-            flapdown = (int)gameObject.transform.rotation.eulerAngles.z;
+            flapdown = (int)gameObject.transform.rotation.eulerAngles.z;// f30
             flapup = 330f; //This pretty much acts as postive -30 in the inspector 
+            vup = Vector3.back;
+            vdown = Vector3.forward;
             Debug.Log("This is Right flap");
         }
     }
@@ -28,16 +36,24 @@ public class FlapScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-           rFlap = (int)gameObject.transform.rotation.eulerAngles.z;
+        rFlap = (int)gameObject.transform.rotation.eulerAngles.z;
+        
     }
     
     public void FlapU()
     {
-
+        if (rFlap != flapup) // 330 > 30
+            {
+                transform.Rotate(vup);
+            }
+ 
     }
 
     public void FlapD()
     {
-
+        if (rFlap != flapdown)
+        {
+            transform.Rotate(vdown);
+        }
     }
 }
